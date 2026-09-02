10e3819711bc3e0b04f892dadfbb79a2f2138e73
femoreno <f.morenov@uniandes.edu.co>
2026-09-02 16:31:30 -0500

    ci: add GitHub Actions workflow

--- src/orders/orders.service.spec.ts
+++ src/orders/orders.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { OrdersService } from './orders.service.js';
+import { Order } from './entities/order.entity.js';
 
 describe('OrdersService', () => {
   let service: OrdersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [
+        OrdersService,
+        {
+          provide: getRepositoryToken(Order),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
